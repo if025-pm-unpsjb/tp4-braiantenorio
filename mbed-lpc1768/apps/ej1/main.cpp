21c1e0c0b6ff85aa1200840ea0df4e6375b7d980
Braian Tenorio <tenoriovidalbraian@gmail.com>
2025-11-13 15:43:33 -0300

    setup y ej1

--- mbed-lpc1768/apps/ej1/main.cpp
+++ mbed-lpc1768/apps/ej1/main.cpp
@@ -1,14 +1,27 @@
 #include "mbed.h"
 
 DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+DigitalOut led3(LED3);
+DigitalOut led4(LED4);
+
+int lead = 0b0000;
 
 int main()
 {
     while(1)
     {
-        led1 = 0;
-        wait(1);
-        led1 = 1;
-        wait(1);
+    	led1 = lead & 0b0001;
+    	led2 = lead & 0b0010;
+    	led3 = lead & 0b0100;
+    	led4 = lead & 0b1000;
+
+    	wait(2);
+
+    	lead += 1;
+
+    	if (lead == 15){
+    		lead = 0;
+    	}
     }
 }
--- mbed-lpc1768/apps/ej2/main.cpp
+++ mbed-lpc1768/apps/ej2/main.cpp
@@ -1,14 +1,25 @@
 #include "mbed.h"
+#include <cmath>
 
-DigitalOut led1(LED1);
+PwmOut led1(LED1);
+PwmOut led2(LED1);
+PwmOut led3(LED1);
+PwmOut led4(LED1);
+
+float lead = 0;
 
 int main()
 {
-    while(1)
-    {
-        led1 = 0;
-        wait(1);
-        led1 = 1;
-        wait(1);
+    while(1){
+    	lead +=0.1;
+        led1 = fmod(lead, 1.0);
+        led2 = fmod(lead, 2.0);
+        led3 = fmod(lead, 3.0);
+        led4= fmod(lead, 4.0);
+        wait(0.10);
+        if (lead < 4.0){
+        	lead = 0
+        }
     }
+
 }
